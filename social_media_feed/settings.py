05d87b0b32841d9f5745e1849730a59e723aeeec
kevin stratvert <usmanrwp51@gmail.com>
2024-11-06 23:58:01 +0500

    commit

--- social_media_feed/settings.py
+++ social_media_feed/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from datetime import timedelta
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv() 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,10 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n*&!firh%lpm)ua$n0rb@e+vcoy$+a!y9ps(=&y%_5xk73vxfu'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+DEBUG = os.getenv('DJANGO_DEBUG', 'False') == 'True' 
 
 ALLOWED_HOSTS = ['18.232.130.29', 'localhost','127.0.0.1']
 
