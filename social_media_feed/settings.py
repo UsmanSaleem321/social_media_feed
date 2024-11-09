3c32a4f11b06504dd474cd9ee727e2a6d2dc2042
kevin stratvert <usmanrwp51@gmail.com>
2024-11-09 11:10:22 +0500

    commit

--- social_media_feed/settings.py
+++ social_media_feed/settings.py
@@ -152,12 +152,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Location where collectstatic will store files
+
+# Additional static file directories
 STATICFILES_DIRS = [
-    BASE_DIR / 'static', 
+    os.path.join(BASE_DIR, 'static'),
 ]
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 LOGIN_URL = '/login/' 
 
 # Default primary key field type
@@ -166,4 +168,4 @@ LOGIN_URL = '/login/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
