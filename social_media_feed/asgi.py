d8abcf1c0fa31cf9a52ca0992ebd1b4602b9151b
kevin stratvert <usmanrwp51@gmail.com>
2024-11-29 23:58:22 +0500

    commit

--- social_media_feed/asgi.py
+++ social_media_feed/asgi.py
@@ -13,7 +13,7 @@ django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
-    "websocket": SessionMiddlewareStack(
+    "websocket": AuthMiddlewareStack(
         CustomAuthMiddleware(
             URLRouter(websocket_urlpatterns)
         )
