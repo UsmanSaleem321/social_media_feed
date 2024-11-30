73312ac3dbe3a96c06bc854f2f5bc0e67b90796a
kevin stratvert <usmanrwp51@gmail.com>
2024-11-30 18:26:59 +0500

    commit

--- social_media_feed/asgi.py
+++ social_media_feed/asgi.py
@@ -4,9 +4,6 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from core.routing import websocket_urlpatterns
-from channels.sessions import SessionMiddlewareStack
-from core.middleware import CustomAuthMiddleware
-
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'social_media_feed.settings')
@@ -15,8 +12,6 @@ django.setup()
 application = ProtocolTypeRouter({
     "http":  get_asgi_application(),
     "websocket": AuthMiddlewareStack(
-        CustomAuthMiddleware(
             URLRouter(websocket_urlpatterns)
-        )
-    ),
+        ),
 })
