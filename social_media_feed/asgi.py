7dd1b79779a1b9a88710bc98475ba87616870436
kevin stratvert <usmanrwp51@gmail.com>
2024-11-30 20:14:43 +0500

    commit

--- social_media_feed/asgi.py
+++ social_media_feed/asgi.py
@@ -1,6 +1,6 @@
 import os
 import django
-django.setup() 
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
@@ -9,7 +9,7 @@ from core.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'social_media_feed.settings')
 # Initialize Django ASGI application early to populate the App Registry
-
+django.setup() 
 application = ProtocolTypeRouter({
     "http":  get_asgi_application(),
     "websocket": AuthMiddlewareStack(
