19d6b3ba872669bdd4cf23d57939a7eb8db8d70d
kevin stratvert <usmanrwp51@gmail.com>
2024-11-30 12:10:47 +0500

    commit

--- social_media_feed/asgi.py
+++ social_media_feed/asgi.py
@@ -5,7 +5,7 @@ from channels.auth import AuthMiddlewareStack
 from core.routing import websocket_urlpatterns
 from channels.sessions import SessionMiddlewareStack
 from core.middleware import CustomAuthMiddleware
-
+from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'social_media_feed.settings')
 # Initialize Django ASGI application early to populate the App Registry
