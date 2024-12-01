e601ea88ef4c58ea9122f60116c98e0aca2b5b76
kevin stratvert <usmanrwp51@gmail.com>
2024-12-01 11:39:48 +0500

    commit

--- social_media_feed/settings.py
+++ social_media_feed/settings.py
@@ -2,27 +2,24 @@ from datetime import timedelta
 import os
 from pathlib import Path
 from dotenv import load_dotenv
-load_dotenv() 
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
-DEBUG = os.getenv('DJANGO_DEBUG', 'False') == 'True' 
+DEBUG = os.getenv('DJANGO_DEBUG', 'False') == 'True'
 VONAGE_API_KEY = os.getenv("VONAGE_API_KEY")
 VONAGE_API_SECRET = os.getenv("VONAGE_API_SECRET")
 
-ALLOWED_HOSTS = ['98.85.161.52', 'localhost','127.0.0.1']
+ALLOWED_HOSTS = ['98.85.161.52', 'localhost', '127.0.0.1']
 
 # Application definition
-
 INSTALLED_APPS = [
-    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.sessions',
@@ -32,15 +29,14 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'channels',
     'rest_framework',
-    'rest_framework_simplejwt', 
+    'rest_framework_simplejwt',
     'rest_framework.authtoken',
     'bootstrap5',
     'core',
-    'api'    
+    'api',
 ]
 
 MIDDLEWARE = [
-    
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -53,12 +49,10 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'social_media_feed.urls'
 
-
-
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,"core/templates/core")],
+        'DIRS': [os.path.join(BASE_DIR, "core/templates/core")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -71,7 +65,6 @@ TEMPLATES = [
     },
 ]
 
-# settings.py
 CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
@@ -84,22 +77,20 @@ CHANNEL_LAYERS = {
 CACHES = {
     "default": {
         "BACKEND": "django.core.cache.backends.redis.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",  # Make sure this matches your Redis setup
+        "LOCATION": "redis://127.0.0.1:6379/1",  # Ensure this matches your Redis setup
     }
 }
 
 SESSION_ENGINE = "django.contrib.sessions.backends.db"
 
-
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(days=20),  # Set access token lifetime (default is 5 minutes)
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),    # Set refresh token lifetime (default is 1 day)
-    'ROTATE_REFRESH_TOKENS': False,                 # Whether to rotate refresh tokens or not
-    'BLACKLIST_AFTER_ROTATION': True,               # Blacklist tokens after refresh
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=20),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS': False,
+    'BLACKLIST_AFTER_ROTATION': True,
 }
 
 REST_FRAMEWORK = {
-    
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
@@ -112,22 +103,18 @@ ASGI_APPLICATION = 'social_media_feed.asgi.application'
 WSGI_APPLICATION = 'social_media_feed.wsgi.application'
 
 # Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'social_media',
         'USER': 'usman',
         'PASSWORD': 'usman',
-        'HOST': 'localhost',  # Use 'localhost' if running on the same server
-        'PORT': '5432',           # Leave empty for default port 5432
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
 # Password validation
-# https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -143,40 +130,24 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
-# https://docs.djangoproject.com/en/4.2/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Location where collectstatic will store files
-
-# Additional static file directories
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-LOGIN_URL = '/login/' 
+LOGIN_URL = '/login/'
 
 # Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-SESSION_COOKIE_SAMESITE = "None"  # For cross-site cookies
-SESSION_COOKIE_SECURE = True     # Required for SameSite=None
+
