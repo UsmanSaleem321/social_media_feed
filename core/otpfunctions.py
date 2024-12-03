615889945ba472e7e4a3158d1a3a0cd8ddd4f9da
kevin stratvert <usmanrwp51@gmail.com>
2024-12-03 21:51:21 +0500

    commit

--- core/otpfunctions.py
+++ core/otpfunctions.py
@@ -1,14 +1,12 @@
-from vonage import Sms
+import nexmo
 
 def send_otp(phone_number, otp):
-    sms = Sms(api_key="fa0d5408", api_secret="VHZOPwnIq8eCVM6I")
-
-    response = sms.send_message({
+    client = nexmo.Client (api_key="fa0d5408", api_secret="VHZOPwnIq8eCVM6I")
+    response =  client.send_message({
         "from": "Vonage",
         "to": phone_number,
         "text": f"Your OTP is: {otp}",
     })
-
     if response["messages"][0]["status"] == "0":
         return True  # OTP sent successfully
     else:
