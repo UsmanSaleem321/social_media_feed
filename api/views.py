7b48282f958283aee699cf7a251e8795e71924ba
kevin stratvert <usmanrwp51@gmail.com>
2024-12-06 22:49:08 +0500

    commit

--- api/views.py
+++ api/views.py
@@ -4,6 +4,10 @@ from rest_framework.permissions import *
 from core.models import Post
 from core.serializer import *
 from .permissions import *
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from core.forms import CustomUserCreationForm
+from rest_framework import status
 
 class PostListCreateApiView(ListCreateAPIView):
     queryset = Post.objects.all()
@@ -45,4 +49,13 @@ class CommentRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
         post = get_object_or_404(Post, id=com_pk)
         comments = Comment.objects.filter(post=post, id=pk)
         return comments
+
+class SignupAPIView(APIView):
+    def post(self, request, *args, **kwargs):
+        form = CustomUserCreationForm(data=request.data)
+        if form.is_valid():
+            form.save()
+            return Response({"message": "User created successfully"}, status=status.HTTP_201_CREATED)
+        else:
+            return Response({"errors": form.errors}, status=status.HTTP_400_BAD_REQUEST)
 
